9f3cd67cbf913b5aa40844d764a99476ca553a00
Сергей <1@php-cat.com>
2025-07-05 23:19:21 +0500

    +102 описание

--- www/bitrix/managed_cache/MYSQL/orm_b_sec_wwall_rules/82/829e168f268d54955d65b868c1387f41.php
+++ www/bitrix/managed_cache/MYSQL/orm_b_sec_wwall_rules/82/829e168f268d54955d65b868c1387f41.php
@@ -1,7 +1,7 @@
 <?
 if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001751738631';
-$dateexpire = '001751738636';
+$datecreate = '001751739539';
+$dateexpire = '001751739544';
 $ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";a:1:{i:0;a:1:{s:3:"CNT";s:1:"0";}}}';
 return true;
 ?>
